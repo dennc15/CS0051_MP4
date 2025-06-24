c6b8c3365748b08252a4b2c4e3453cf0724394a5
Kreipri <beaganotisi@gmail.com>
2025-06-24 21:41:22 +0800

    finally gumana na

--- MP4_GROUP4.cpp
+++ MP4_GROUP4.cpp
@@ -27,62 +27,13 @@ vector<Question> questions = { //SAMPLE QUESTIONS
     {"Who wrote 'Romeo and Juliet'?", 'a', {"a) William Shakespeare", "b) Charles Dickens", "c) Jane Austen", "d) Mark Twain"}}
 };
 
-void gameplay(int id, latch& login, barrier& finishRound, bool isAuto){
-    int ch;
-    
-    {
-        lock_guard<mutex> lock(printMtx);
-        cout<<"Player "<< id<<" Logging in.."<<endl;
-    }
-    //login use countdown and print when player has logged in
-    this_thread::sleep_for(chrono::seconds(1)); //temporary latch
-    //"All players logged in!"
-    for (int i = roundNo; i <= 3; ++i){
-        {
-            lock_guard<recursive_mutex> printMtx;
-            ch = askQ();
-            cout<<"Player "<<id<<" has chosen "<<ch<<"!"<<endl;
-        }
-        finishRound.arrive_and_wait();
-        
-    }
-
-}
-
-void start(int numPlayers, bool isAuto){
-    latch login(numPlayers);
-    barrier finishRound(numPlayers, [](){
-       cout<<"[ROUND "<<roundNo<<" FINISHED]"<<endl;;
-       cout<<"Checking scores..."<<endl;
-    });
-
-
-    vector<thread> players;
-    for(int i = 0; i < numPlayers; ++i) players.emplace_back(gameplay, i+1, ref(login), ref(finishRound), isAuto); //start threads
-    this_thread::sleep_for(chrono::seconds(1)); //temporary latch
-    
-    cout<<"All players logged in!";
-    
-
-
-    for (auto& t : players) t.join(); //join threads
-}
-
-
-char askQ(){ //asks questions and calls lifeline
-    char ch;
-    Question q = questions[roundNo-1];
-    cout<<q.q<<endl;
-    for (auto& choice : q.ch)cout<<choice<<endl;
-    cout<<"Choice: ";
-    cin>>ch;
-    //add lifeline here, lifeline will return the ch that this method will be returning
-    return ch;
-}
-
-char lifeline(){
+//PROTOTYPES
+char askQ();
+char lifeline();
     //lifeline menu uses rng
-}
+void gameplay(int id, latch& login, barrier<>& finishRound, bool isAuto);
+void start(int numPlayers, bool isAuto);
+
 
 int main()
 {
@@ -96,11 +47,70 @@ int main()
     cin>>ch;
 
     switch(ch){
-        case 2: 
+        case 2: //Manual
             cout<<"Num players: ";
             cin>>numPlayers;
             start(numPlayers, false);
             
     }
+}
+
+char askQ(){ //asks questions and calls lifeline, add automated version too either in this function or in a diff function or sa gameplay function
+    char ch;
+    Question q = questions[roundNo-1];
+    cout<<q.q<<endl;
+    for (auto& choice : q.ch)cout<<choice<<endl;
+    cout<<"Choice: ";
+    cin>>ch;
+    //add lifeline here, lifeline will return the ch that this method will be returning
+    return ch;
+}
+
+
+
+
+void gameplay(int id, latch& login, barrier<function<void()>>& finishRound, bool isAuto){
+    int ch;
+    
+    {
+        lock_guard<recursive_mutex> lock(printMtx);
+        cout<<"Player "<< id<<" Logging in.."<<endl;
+    }
+    //login use countdown and print when player has logged in
+    this_thread::sleep_for(chrono::seconds(1)); //temporary latch
+    //"All players logged in!"
+    for (int i = roundNo; i <= 3; ++i){
+        {
+            lock_guard<recursive_mutex> lock(printMtx);
+            ch = askQ();
+
+            cout<<"Player "<<id<<" has chosen "<<ch<<"!"<<endl;
+        }
+        finishRound.arrive_and_wait();
+    }
+
+}
+
+void start(int numPlayers, bool isAuto){
+    latch login(numPlayers);
+    barrier<function<void()>> finishRound(numPlayers, [](){
+       cout<<"[ROUND "<<roundNo<<" FINISHED]"<<endl;
+       cout<<"Checking scores..."<<endl;
+    });
+
+
+    vector<thread> players;
+    for(int i = 0; i < numPlayers; ++i) 
+    players.emplace_back([=, &login, &finishRound]() {
+        gameplay(i + 1, login, finishRound, isAuto);
+    }); //start threads
+    
+    this_thread::sleep_for(chrono::seconds(1)); //temporary latch
+    
+    cout<<"All players logged in!";
+    
+
+
+    for (auto& t : players) t.join(); //join threads
 }
 
--- NOTES.md
+++ NOTES.md
@@ -1,4 +1,5 @@
-/*
+sample output:
+
 log in users
 
 round 1:
@@ -69,44 +70,3 @@ Division of parts:
     Overall - add mutexes for printing
 
 functions:
-    simulation //initialize threads here----
-        thread t1(gameplayAutomated, 1);
-        t1.join();
-    void gameplayAutomated //no input needed----
-    void gameplayManual //user input for all players ---- {
-        questions = [
-            ["Q: sdaada","a"],
-            ["Q2: sdadsdad", "b"]
-        ] 
-        for (q : question){
-            ans = question(q)
-            bool tf = async(checking, ans)
-            print checking ans
-            if(tf){
-                plus points
-                print player # is correct
-            }
-            else{
-                print player # is incorrect
-            }
-        }
-    }
-    void Question(string [] q) //calls lifeline here
-        print q[0]
-        choice:
-        //start timer
-        if(ch == 0){
-            ch = lifeline();
-        }
-        //repeat question if answer is not valid
-        
-        return ch;
-    
-    string Lifeline() //use rng
-    bool checking(answer)
-    void addScore(){
-        if checking()
-    }
-
-    dk if we should use string or char for answer
-*/
