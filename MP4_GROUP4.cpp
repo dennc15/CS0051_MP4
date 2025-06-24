59c954ada64c6c2f19d9fcacff4a01261da195f2
Kreipri <beaganotisi@gmail.com>
2025-06-24 23:33:43 +0800

    functional

--- MP4_GROUP4.cpp
+++ MP4_GROUP4.cpp
@@ -13,9 +13,12 @@ using namespace std;
 recursive_mutex printMtx;
 int roundNo = 1;
 latch login(4);
+int points[4] = {0,0,0,0};
+bool correct[4] = {0,0,0,0};
+
 barrier finishRound(4, [](){
     lock_guard<recursive_mutex> lock(printMtx);
-    cout<<"[ROUND "<<roundNo<<" FINISHED]"<<endl;
+    cout<<"\n[ROUND "<<roundNo<<" FINISHED]"<<endl;
     cout<<"Checking scores..."<<endl;
     for(int i=0; i<4; i++){
         if(correct[i]){
@@ -25,10 +28,10 @@ barrier finishRound(4, [](){
         }
     }
     cout<<"\nCurrent Scores:"<<endl;
-    for(int i=0; i<4; i++)cout<<"Player "<<i+1<<"-- "points[i]<<endl;
+    for(int i=0; i<4; i++)
+        cout<<"Player "<<i+1<<" -- "<<points[i]<<endl;
     
-
-    
+    roundNo++;
 });
 
 struct Question{
@@ -37,11 +40,7 @@ struct Question{
     vector<string> ch;
 };
 
-int points[4] = {0,0,0,0};
-bool correct[4] = {0,0,0,0};
-
-
-vector<Question> questions = { //SAMPLE QUESTIONS
+vector<Question> questions = { //SAMPLE QUESTIONS <------------------- TODO
     {"What is 2 + 2?", 'a', {"a) 4", "b) 3", "c) 5", "d) 22"}},
     {"Capital of France?", 'b', {"a) Rome", "b) Paris", "c) London", "d) Berlin"}},
     {"Which planet is known as the Red Planet?", 'c', {"a) Venus", "b) Jupiter", "c) Mars", "d) Mercury"}},
@@ -57,6 +56,7 @@ void gameplay(int id, bool isAuto);
 void start(int numPlayers, bool isAuto);
 bool checkAns(int id, char ans); 
     //check if true and cout if player is right<---------------------- TODO
+string checkHighest();
 
 
 int main()
@@ -64,10 +64,10 @@ int main()
     int ch, numPlayers;
 
     cout<<"MENU"<<endl;
-    cout<<"1. Automatic"<<endl;
+    cout<<"1. Automatic"<<endl; //OPTIONAL NALANG 
     cout<<"2. Manual"<<endl;
 
-    //add error handling and exit
+    //add error handling and exit <------------------- TODO
     cin>>ch;
 
     switch(ch){
@@ -77,23 +77,25 @@ int main()
     }
 }
 
-char askQ(int id){ //asks questions and calls lifeline, add automated version too either in this function or in a diff function or sa gameplay function
+char askQ(int id){ //<------------------- TODO asks questions and calls lifeline, add automated version too either in this function or in a diff function or sa gameplay function
     char ch;
     Question q = questions[roundNo-1];
     cout<<q.q<<endl;
     for (auto& choice : q.ch)cout<<choice<<endl;
     cout<<"Player "<<id<<"'s choice: ";
     cin>>ch;
-    //add lifeline here, lifeline will return the ch that this method will be returning
+    //add lifeline here, lifeline will return the ch that this method will be returning <------------------- TODO
     return ch;
 }
 
-bool checkAns(int id) {
-    char correct = questions[roundNo - 1].ans;
-    if (answer == correct) {
+bool checkAns(int id, char ch) {
+    char cor = questions[roundNo - 1].ans;
+    if (ch == cor) {
         points[id - 1] += 10;
+        correct[id - 1] = 1;
         return true;
     } else {
+        correct[id - 1] = 0;
         return false;
     }
 }
@@ -105,24 +107,48 @@ void gameplay(int id, bool isAuto){
         lock_guard<recursive_mutex> lock(printMtx);
         cout<<"Player "<< id<<" Logging in.."<<endl;
     }
-    //login use countdown and print when player has logged in
+    //login use countdown and print when player has logged in <------------------------- TODO
     this_thread::sleep_for(chrono::seconds(1)); //temporary latch
     //"All players logged in!"
     for (int i = roundNo; i <= 3; ++i){
         {
             lock_guard<recursive_mutex> lock(printMtx);
-            cout<<"[ROUND "<<roundNo<<" -- Player "<<id<<"'s turn]";
+            cout<<"\n[ROUND "<<roundNo<<" -- Player "<<id<<"'s turn]"<<endl;
             ch = askQ(id);
 
-            cout<<"Player "<<id<<" has chosen "<<ch<<"!"<<endl;
+            //cout<<"Player "<<id<<" has chosen "<<ch<<"!"<<endl; print this in automated
             
         }
         //async
-        future<bool> res = async(launch::async, checkAns, id);
+        future<bool> res = async(launch::async, checkAns, id, ch);
         finishRound.arrive_and_wait();
         
     }
 
+}
+
+string checkHighest(){
+    int max = points[0];
+    vector<int> top = {0};
+
+    for (int i = 1; i < 4; ++i) {
+        if (points[i] > max) {
+            max = points[i];
+            top.clear();
+            top.push_back(i);
+        } else if (points[i] == max) {
+            top.push_back(i);
+        }
+    }
+
+    string winners;
+    for (size_t i = 0; i < top.size(); ++i) {
+        winners += "Player " + to_string(top[i] + 1);
+        if (i < top.size() - 1)
+            winners += " and ";
+    }
+
+    return winners;
 }
 
 void start(int numPlayers, bool isAuto){
@@ -133,9 +159,11 @@ void start(int numPlayers, bool isAuto){
 
     this_thread::sleep_for(chrono::seconds(1)); //temporary latch
     
-    cout<<"All players logged in!";
+    //cout<<"All players logged in!"; print when latch is goods <------------------- TODO
     
     for (auto& t : threads) t.join();
+    
+    cout<<"The Player(s) with the highest score is.. "<<checkHighest()<<"!"<<endl;
 
 }
 
